76011ce3d8533964e5234887712522312c74eccc
Grant Maloney <grantmaloney@ATM0561694DYN.dhcp.missouri.edu>
2019-01-31 13:21:16 -0600

    Initial commit

---
+++ SpeechRecognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechRecognition
+//
+//  Created by Grant Maloney on 1/31/19.
+//  Copyright © 2019 Grant Maloney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
